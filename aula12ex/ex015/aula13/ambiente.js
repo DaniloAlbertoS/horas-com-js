461906f0390b22c390e9097b770b38c67149a4c6
Danilo Alberto <danialberto42@gmail.com>
2022-11-03 09:54:28 -0300

    criado estruturas de repetição

---
+++ aula12ex/ex015/aula13/ambiente.js
@@ -0,0 +1,24 @@
+var c = 1
+do{
+    console.log(`informaçao ${c}`)
+    c++
+} while(c<=10)
+// esta de cima e uma estrutura de repetição no final o do(faça). executa o codigo e depois verifica o while //
+
+
+/*while (c <=10){
+    console.log(`informação ${c}`)
+    c++ 
+    //esse é  o modo de  repetição ou seja  o while(quando)estrutura de repeticao  no inicio. esta primeiro ve se o  while e depois execulta a função.
+    essa é  a mais encontrada nos  progrmas.//
+}*/
+
+
+
+
+/*console.log('tudo bem ?')
+console.log('tudo bem ?')
+console.log('tudo bem ?')
+console.log('tudo bem ?')
+console.log('tudo bem ?')
+console.log('tudo bem ?')*/
